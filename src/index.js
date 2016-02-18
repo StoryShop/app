1dcf9e11d48ff9421be3c6aaea995adae7a838e4
Josh David Miller <josh@joshdmiller.com>
2016-02-18 14:14:06 -0800

    refactor: update react-router history usage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { Router } from 'react-router';
-import createHistory from 'history/lib/createBrowserHistory';
+import { Router, useRouterHistory } from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
 
-const history = createHistory({ queryKey: false });
+const history = useRouterHistory(createBrowserHistory)({ queryKey: false });
 
 injectTapEventPlugin();
 
