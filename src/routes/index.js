23cfe5da12a9029c5ceb2da0570779e2efad061b
Seth Atwood <seth@strangewindstudio.com>
2016-06-06 01:44:35 -0400

    feat: improved beta route view

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,6 +17,29 @@ const App = AppFactory(
   withUiStore
 );
 
+const styles = {
+  betaWrap: {
+    width: '50%',
+    margin: '5% auto 0',
+    padding: '2em',
+    background: '#0e4254',
+    borderRadius: '1em'
+  },
+  logoImg: {
+    display: 'block',
+    maxWidth: '80%',
+    margin: '0 auto'
+  },
+  betaContent: {
+    marginTop: '2em',
+    padding: '1em',
+    background: 'white',
+    fontFamily: 'Roboto, sans-serif',
+    fontSize: '1.4em',
+    borderRadius: '1em'
+  }
+};
+
 export default {
   path: '/',
   component: ({ children }) => children,
@@ -33,7 +56,15 @@ export default {
       path: 'beta',
       component: () => {
         return (
-          <p>StoryShop is currently in private beta.</p>
+          <div style={styles.betaWrap}>
+            <div>
+              <img style={styles.logoImg} src="http://getstoryshop.com/img/logo.png" />
+              <div style={styles.betaContent}>
+                <p>StoryShop is currently in private beta.</p>
+                <p>If you are a beta user and believe you are seeing this message in error, please be sure to check the app status at <a href="http://status.storyshopapp.com">http://status.storyshopapp.com</a>, before contacting us for support through <a href="http://feedback.storyshopapp.com">http://feedback.storyshopapp.com</a>. Thank you!</p>
+              </div>
+            </div>
+          </div>
         );
       },
     },
