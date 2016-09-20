6ddce518a1cfd23e397914d1bf78a9aa4c20b122
Chris Harrington <chrisharrington99@gmail.com>
2016-09-19 20:14:04 -0600

    feat: character relationship component

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,14 @@ module.exports = {
     }),
   ],
 
+  externals: {
+    'react/lib/ExecutionEnvironment': true
+    // 'react': 'React',
+    // 'react-dom': 'ReactDOM',
+    // 'react-router': 'ReactRouter',
+    // 'history': 'History',
+  },
+
   module: {
     loaders: [
       {
