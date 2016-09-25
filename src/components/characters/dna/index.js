6e6371b567efced89123d62f30c70e7bea7b2fb3
Chris Harrington <chrisharrington99@gmail.com>
2016-09-24 19:16:40 -0600

    feat: added hover delete buttons for each gene in the DNA component.
    Closes #88.

--- src/components/characters/dna/index.js
+++ src/components/characters/dna/index.js
@@ -1,39 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reactStamp from 'react-stamp';
 import Paper from 'material-ui/lib/paper';
 import FlatButton from 'material-ui/lib/flat-button';
 import IconButton from 'material-ui/lib/icon-button';
+import TrashIcon from 'material-ui/lib/svg-icons/action/delete';
 import AddIcon from 'material-ui/lib/svg-icons/content/add';
 import PlayIcon from 'material-ui/lib/svg-icons/av/skip-next';
 import * as colours from 'material-ui/lib/styles/colors';
 import { FlexLayout } from 'components/flex';
 import InlineEdit from 'components/inline-edit';
 
-export default reactStamp( React ).compose({
-  init  () {
+export default class Dna extends React.Component {
+  constructor  (props) {
+    super();
     this.state = {
-      newGene: this.props.randomGene,
+      newGene: props.randomGene,
       adding: true,
+      hover: {}
     };
-  },
+  };
 
   _add () {
     this.setState({ adding: true, newGene: { gene: '', allele: '' } });
-  },
+  };
 
   _onNewBlur ( newGene ) {
     if ( newGene.allele && newGene.allele !== '' ) {
       this.props.addGene( this.props.id, newGene );
     }
-  },
+  };
 
   componentWillReceiveProps ( newProps ) {
     this.setState({
       adding: newProps.genes.length === this.props.genes.length,
       newGene: newProps.randomGene,
     });
-  },
+  };
 
   render () {
     const {
@@ -57,24 +59,36 @@ export default reactStamp( React ).compose({
       },
 
       rows: {
-        marginBottom: '8px',
+        marginBottom: '8px'
       },
 
       row: {
         margin: '0 0 16px 0',
+        position: 'relative'
       },
 
       gene: {
         marginBottom: 0,
         color: '#999',
         fontSize: '0.85em',
+        paddingRight: 48
       },
 
       allele: {
+        paddingRight: 48
       },
 
       footer: {
       },
+
+      remove: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        cursor: 'pointer',
+        opacity: 0,
+        transition: 'opacity 200ms ease-in'
+      }
     };
 
     styles.newRow = {
@@ -89,8 +103,10 @@ export default reactStamp( React ).compose({
       .sort()
       .reverse()
       .map( k => ({ idx: k, gene: genes[ k ] }) )
-      .map( ({ idx, gene }) => (
-        <div key={idx} style={styles.row} direction="column">
+      .map( ({ idx, gene }) => {
+        let style = Object.assign({}, styles.remove);
+        style.opacity = this.state.hover[gene.id] ? 1 : 0;
+        return <div key={idx} style={styles.row} className="dna-wrapper" direction="column" onMouseOver={this.onRowHover.bind(this, gene, true)} onMouseOut={this.onRowHover.bind(this, gene, false)}>
           <InlineEdit
             style={styles.gene}
             flex="40"
@@ -104,9 +120,14 @@ export default reactStamp( React ).compose({
             value={gene.allele}
             onChange={val => changeGene( id, idx, gene, 'allele', val )}
           />
+
+          <IconButton
+            style={style}
+            onClick={this.deleteGene.bind(this, idx, gene)}>
+            <TrashIcon />
+          </IconButton>
         </div>
-      ))
-      ;
+      });
 
     const newRow = (
       <div key={rows.length} style={styles.newRow} direction="column">
@@ -161,9 +182,9 @@ export default reactStamp( React ).compose({
         </FlexLayout>
       </FlexLayout>
     );
-  },
+  };
 
-  statics: {
+  statics = {
     modelPaths ( conf ) {
       const { prefix, pagination } = conf;
 
@@ -172,6 +193,17 @@ export default reactStamp( React ).compose({
         [ 'genes', 'random' ],
       ];
     },
-  },
-});
+  };
 
+  onRowHover(gene, isHover) {
+    let hover = this.state.hover;
+    hover[gene.id] = isHover;
+    this.setState({
+      hover: hover
+    });
+  };
+
+  deleteGene(idx, gene) {
+    this.props.deleteGene(idx, gene);
+  };
+};
